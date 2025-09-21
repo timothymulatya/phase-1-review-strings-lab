54205f9cb5ee70f96c13002313389a1821b763fc
timothymulatya <timothymulatya@gmail.com>
2025-09-21 12:38:26 +0300

    push

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+// interpolation
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// backticks allow us to interpolate with ${}
+// .sclice() allows us to return specific characters
+const shortGreeting =`Welcome, ${currentUser.slice(0,1)}!`;
